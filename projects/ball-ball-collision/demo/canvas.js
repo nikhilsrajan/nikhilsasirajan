1c0d06f91960466125f0bdbb030d3503b1958140
Nikhil Sasi Rajan <nikhilsasirajan@gmail.com>
2020-04-30 08:18:02 +0530

    added some constants and classes

--- projects/ball-ball-collision/demo/canvas.js
+++ projects/ball-ball-collision/demo/canvas.js
@@ -10,7 +10,6 @@ window.addEventListener('resize',
     function(e) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        circles_array.reset();
     }
 );
 
@@ -20,6 +19,32 @@ function clearCanvas() {
     c.clearRect(0, 0,  canvas.width, canvas.height);
 }
 
+// ---------------------------------------------------
+
 var frame_rate = 1/40; // seconds
 var frame_delay = frame_rate * 1000; // ms
+var gravity = 9.8; // m / s^2
 
+// ---------------------------------------------------
+
+class ball {
+    constructor(radius, mass, position, velocity) {
+        this.radius = radius;
+        this.mass = mass;
+        this.pos = {x: position.x, y: position.y};
+        this.vel = {x: velocity.x, y: velocity.y};
+    }
+};
+
+// ---------------------------------------------------
+
+function draw(ball) {
+    c.beginPath();
+    c.arc(ball.pos.x, ball.pos.y, ball.pos.radius, 0, Math.PI * 2, true);
+    c.strokeStyle = 'rgba(255, 50, 50, 0.5)';
+    c.stroke();
+    c.fillStyle = c.strokeStyle;
+    c.fill();
+}
+
+// ---------------------------------------------------
